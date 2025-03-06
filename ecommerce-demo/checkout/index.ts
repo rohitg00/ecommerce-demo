3aec04c39ee716001cca30775f8c0e92977519c8
Eden <eden@bit.dev>
2025-03-06 16:45:47 +0200

    explicitly mention interface exports as type exports

--- ecommerce-demo/checkout/index.ts
+++ ecommerce-demo/checkout/index.ts
@@ -3,6 +3,6 @@ import { CheckoutAspect } from './checkout.aspect.js';
 export type { CheckoutConfig } from './checkout-config.js';
 export { CheckoutNode } from './checkout.node.runtime.js';
 export { CheckoutBrowser } from './checkout.browser.runtime.js';
-export { MenuItem, MenuItemSlot } from './menu-item.js';
-
+export { type MenuItem, type MenuItemSlot } from './menu-item.js';
+export { CheckoutAspect };
 export default CheckoutAspect;
--- ecommerce-demo/user/index.ts
+++ ecommerce-demo/user/index.ts
@@ -3,6 +3,6 @@ import { UserAspect } from './user.aspect.js';
 export type { UserConfig } from './user-config.js';
 export { UserNode } from './user.node.runtime.js';
 export { UserBrowser } from './user.browser.runtime.js';
-export { MenuItem, MenuItemSlot } from './menu-item.js';
-
+export type { MenuItem, MenuItemSlot } from './menu-item.js';
+export { UserAspect };
 export default UserAspect;
